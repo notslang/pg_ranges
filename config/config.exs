e4a2514cbec3e81cd7162ceabc8cedb01d5249bb
Sean Lang <slang800@gmail.com>
2022-10-28 21:17:20 -0500

    update pg username, password, and database name

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,9 @@ if config_env() == :test do
   config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
   config :pg_ranges, PgRanges.Repo,
-    username: "pgranges",
-    password: "pgranges",
-    database: "pgranges",
+    username: "postgres",
+    password: "postgres",
+    database: "pg_ranges",
     hostname: "localhost",
     pool_size: 10,
     pool: Ecto.Adapters.SQL.Sandbox
